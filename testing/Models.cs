ed27f2079036b071f190feca86efe1261fa73b06
Alireza Ahmadi <a.ahmadi@papgroup.local>
2021-01-18 15:45:12 +0330

    Add project files.

---
+++ testing/Models.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace testing
+{
+    class WorkReport
+    {
+        public WorkReport()
+        {
+        }
+        public WorkReport(string employeeId,DateTime date)
+        {
+            Date = date;
+            EmployeeId = employeeId;
+        }
+        public DateTime Date { get; set; }
+        public string EmployeeId { get; set; }
+    }
+    class Activity
+    {
+        public Activity(TimeSpan startTime,TimeSpan endTime)
+        {
+            StartTime = startTime;
+            EndTime = endTime;
+            TotalTime = EndTime - StartTime;
+        }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public string TotalTime { get; set; }
+        public string Notes { get; set; }
+    }
+}
---
+++ testing/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myDate = new DateTime()
+            TimeSpan timeSpan = new TimeSpan(2, 45, 50);
+            TimeSpan timeSpan1 = new TimeSpan()
+                
+           
+        }
+    }
+}
